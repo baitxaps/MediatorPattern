84adb820d511705dca4fd5967bc406b2213ffb25
rhc <chenhairong@eelly.net>
2016-07-22 18:12:36 +0800

    abstructor

---
+++ MediatorPattern/AbstractClass/AbstractMediator.h
@@ -0,0 +1,13 @@
+//
+//  AbstractMediator.h
+//  MediatorPattern
+//
+//  Created by hairong.chen on 16/7/22.
+//  Copyright © 2016年 hairong.chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AbstractMediator : NSObject
+
+@end
---
+++ MediatorPattern/AbstractClass/AbstractMediator.m
@@ -0,0 +1,37 @@
+//
+//  AbstractMediator.m
+//  MediatorPattern
+//
+//  Created by hairong.chen on 16/7/22.
+//  Copyright © 2016年 hairong.chen. All rights reserved.
+//
+
+#import "AbstractMediator.h"
+#import "Purchase.h"
+#import "Sale.h"
+#import "Stock.h"
+
+@interface AbstractMediator()
+
+@property (nonatomic ,strong) Purchase *purchase;
+@property (nonatomic ,strong) Sale *sale;
+@property (nonatomic ,strong) Stock *stock;
+
+@end
+
+@implementation AbstractMediator
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _purchase = [[Purchase alloc]init];
+        _sale = [[Sale alloc]init];
+        _stock = [[Stock alloc]init];
+    }
+    return self;
+}
+
+- (void)execute:(NSString *)str object:(id)cobj {
+    
+}
+
+@end
